e617c2fbc20f11100c1459f37dc51c99a84efe6c
y00eunji <aine0227@naver.com>
2023-06-20 21:07:07 +0900

    design: 마이페이지 마크업 및 스타일링

--- front/src/page/MyPage/style.js
+++ front/src/page/MyPage/style.js
@@ -1,6 +1,44 @@
 import styled from 'styled-components';
 
-
 export const MyPageContainer = styled.div`
-    /* background-color: black; */
-`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+export const H2 = styled.h2`
+  font-size: 1rem;
+`;
+
+export const MyPageProfileContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 2rem;
+`;
+
+export const MyPageProfileImage = styled.img`
+  width: 30%;
+  height: 30%;
+  border-radius: 50%;
+  object-fit: cover;
+  border: 1px solid #ccc;
+`;
+
+export const MyPageNickname = styled.h2`
+  font-size: 1.5rem;
+  margin-top: 1rem;
+`;
+
+export const MyPageButton = styled.button`
+  padding: 1rem;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  text-align: left;
+  font-size: 1rem;
+  border-bottom: 2px solid #ccc;
+
+  &:hover {
+    border-color: #999;
+  }
+`;
