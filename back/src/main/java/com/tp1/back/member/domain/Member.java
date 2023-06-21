958a8ce7b8a51a820bad290a82b80379aa9a7519
Daeeun Ko <7273322@naver.com>
2023-06-21 22:12:50 +0900

    feat: 이메일 유효성 검사

--- back/src/main/java/com/tp1/back/member/domain/Member.java
+++ back/src/main/java/com/tp1/back/member/domain/Member.java
@@ -5,12 +5,16 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Getter
 @Entity
 @Table(name = "member")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member extends BaseEntity {
+
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[a-z0-9._-]+@[a-z]+[.]+[a-z]{2,3}$");
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,7 +28,15 @@ public class Member extends BaseEntity {
     private String password;
 
     public Member(String email, String password) {
+        validateEmail(email);
         this.email = email;
         this.password = password;
+    }
+
+    private void validateEmail(String email) {
+        Matcher matcher = EMAIL_PATTERN.matcher(email);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("이메일 형식이 올바르지 않습니다.");
+        }
     }
 }
---
+++ back/src/test/java/com/tp1/back/member/domain/MemberTest.java
@@ -0,0 +1,20 @@
+package com.tp1.back.member.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static com.tp1.back.common.fixtures.MemberFixtures.AINE_PASSWORD;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class MemberTest {
+
+    @DisplayName("유효하지 않은 이메일로 멤버 생성 시 예외 발생")
+    @ParameterizedTest
+    @ValueSource(strings = {"test", "test@", "testtestcom", "test.test@test", "@test.com"})
+    void 유효하지_않은_이메일로_멤버_생성_시_예외_발생(final String email) {
+        assertThatThrownBy(() -> new Member(email, AINE_PASSWORD))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
