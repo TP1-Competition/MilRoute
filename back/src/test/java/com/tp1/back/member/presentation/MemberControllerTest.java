98f517fb01d0f39894fe66357b0cdbaaedb4c338
Daeeun Ko <7273322@naver.com>
2023-06-25 04:25:06 +0900

    feat: 회원가입 API 변경

--- back/src/test/java/com/tp1/back/member/presentation/MemberControllerTest.java
+++ back/src/test/java/com/tp1/back/member/presentation/MemberControllerTest.java
@@ -1,19 +1,24 @@
 package com.tp1.back.member.presentation;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tp1.back.auth.application.AuthenticationFilter;
 import com.tp1.back.common.fixtures.MemberFixtures;
 import com.tp1.back.member.application.MemberService;
 import com.tp1.back.member.dto.RegisterRequest;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static com.tp1.back.common.fixtures.MemberFixtures.AINE;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -21,17 +26,22 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@WebMvcTest({MemberController.class})
+@WebMvcTest(controllers = MemberController.class,
+        excludeFilters = @ComponentScan.Filter(
+                type = FilterType.ASSIGNABLE_TYPE,
+                classes = {AuthenticationFilter.class}
+        )
+)
 class MemberControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
-    @MockBean
-    private MemberService memberService;
-
     @Autowired
     private ObjectMapper objectMapper;
+
+    @MockBean
+    private MemberService memberService;
 
     @DisplayName("회원가입 요청 성공")
     @WithMockUser
@@ -42,7 +52,7 @@ class MemberControllerTest {
         given(memberService.register(any(RegisterRequest.class)))
                 .willReturn(true);
 
-        mvc.perform(post("/api/v1/users")
+        mvc.perform(post("/api/v1/users/register")
                         .content(objectMapper.writeValueAsString(request))
                         .contentType(MediaType.APPLICATION_JSON)
                         .with(csrf())
@@ -52,14 +62,13 @@ class MemberControllerTest {
 
     @DisplayName("잘못된 정보로 회원가입 요청 시 실패")
     @WithMockUser
-    @ParameterizedTest
-    @EnumSource
-    void givenInvalidRequest_whenRegister_thenFailWith400(final MemberFixtures memberFixtures) throws Exception {
-        RegisterRequest request = new RegisterRequest(memberFixtures.email, "1");
+    @Test
+    void givenInvalidRequest_whenRegister_thenFailWith400() throws Exception {
+        RegisterRequest request = new RegisterRequest(AINE.email, "1");
         given(memberService.register(any(RegisterRequest.class)))
                 .willThrow(IllegalArgumentException.class);
 
-        mvc.perform(post("/api/v1/users")
+        mvc.perform(post("/api/v1/users/register")
                         .content(objectMapper.writeValueAsString(request))
                         .contentType(MediaType.APPLICATION_JSON)
                         .with(csrf())
