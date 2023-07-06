34b944f6f3dbd99d1ae696781a775d1fdd79b5cf
Daeeun Ko <7273322@naver.com>
2023-07-06 10:12:39 +0900

    feat: 경로, 장소, 길, 하위 길 스키마 생 및 수정

--- back/src/main/java/com/tp1/back/path/domain/Path.java
+++ back/src/main/java/com/tp1/back/path/domain/Path.java
@@ -1,30 +1,66 @@
 package com.tp1.back.path.domain;
 
+import com.tp1.back.place.domain.Place;
 import com.tp1.back.route.domain.Route;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Entity
 @Table(name = "path")
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "path_type", discriminatorType = DiscriminatorType.STRING)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Path {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
     private Long id;
 
-    @Column(name = "distance")
-    private int distance;
-
-    @Column(name = "time")
-    private int time;
+    @ManyToOne
+    @JoinColumn(name = "route_id")
+    private Route route;
 
     @ManyToOne
-    @JoinColumn(name = "path_id")
-    Route route;
+    @JoinColumn(name = "start_place_id")
+    private Place startPlace;
+
+    @ManyToOne
+    @JoinColumn(name = "end_place_id")
+    private Place endPlace;
+
+    @Column(name = "map_obj")
+    private String mapObj;
+
+    @OneToMany(mappedBy = "path")
+    private List<SubPath> subPaths = new ArrayList<>();
+
+    @Builder
+    private Path(Route route, Place startPlace, Place endPlace, String mapObj, List<SubPath> subPaths) {
+        this.route = route;
+        this.startPlace = startPlace;
+        this.endPlace = endPlace;
+        this.mapObj = mapObj;
+    }
+
+    public int getTotalTime() {
+        return subPaths.stream()
+                .mapToInt(SubPath::getTime)
+                .sum();
+    }
+
+    public int getTotalDistance() {
+        return subPaths.stream()
+                .mapToInt(SubPath::getDistance)
+                .sum();
+    }
+
+    public int getTotalPayment() {
+        return 0;
+    }
 }
--- back/src/main/java/com/tp1/back/route/domain/Route.java
+++ back/src/main/java/com/tp1/back/route/domain/Route.java
@@ -2,12 +2,15 @@ package com.tp1.back.route.domain;
 
 import com.tp1.back.common.BaseEntity;
 import com.tp1.back.member.domain.Member;
+import com.tp1.back.path.domain.Path;
 import com.tp1.back.routeplace.domain.RoutePlace;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -21,16 +24,18 @@ public class Route extends BaseEntity {
     @Column
     private Long id;
 
-    @Column
-    private String mapObj;
-
-    @Column
-    private int payment;
-
     @ManyToOne
     @JoinColumn(name = "member_id")
     private Member member;
 
     @OneToMany(mappedBy = "route")
-    private List<RoutePlace> routePlaces;
+    private List<RoutePlace> routePlaces = new ArrayList<>();
+
+    @OneToMany(mappedBy = "route")
+    private List<Path> paths = new ArrayList<>();
+
+    @Builder
+    Route(Member member) {
+        this.member = member;
+    }
 }
